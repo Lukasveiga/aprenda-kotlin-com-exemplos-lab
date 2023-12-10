67f98ed49b7034d81ac8e0a3873684eda4bf2943
Lukas Veiga <lukas.veiga@hotmail.com>
2023-12-10 11:06:37 -0300

    Desafio finalizado

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,54 @@
+package desafio
+
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario(val nome: String, val email: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+class Formacao(private val nome: String, private val nivel: Nivel, private val conteudos: List<ConteudoEducacional>) {
 
-    val inscritos = mutableListOf<Usuario>()
-    
+    private val inscritos = mutableListOf<Usuario>()
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        if(emailJaCadastrado(usuario)) {
+            return println("Email (${usuario.email}) já cadastrado")
+        }
+
+        inscritos.add(usuario)
+        println("Usuário ${usuario.nome} matriculado com sucesso!")
+    }
+
+    private fun emailJaCadastrado(usuario: Usuario): Boolean {
+        return this.inscritos.any {it.email == usuario.email};
+    }
+
+    fun mostrarInfoFormacao() {
+        val info = StringBuilder("Nome: $nome\nNível: $nivel\nNª Alunos Matriculados: ${this.inscritos.size}\nConteúdos Educacionais:\n")
+        conteudos.forEach {c -> info.append("- ${c.nome}\n")}
+
+        println(info.toString())
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("Usuario 1", "usuario1@email.com")
+    val usuario2 = Usuario("Usuario 2", "usuario2@email.com")
+
+    val listContEduc = mutableListOf(
+        ConteudoEducacional("Cont 1"),
+        ConteudoEducacional("Cont 2"),
+        ConteudoEducacional("Cont 3")
+    )
+
+    val form = Formacao("Form 1", Nivel.BASICO, listContEduc)
+    form.mostrarInfoFormacao()
+
+    form.matricular(usuario1)
+    form.matricular(usuario1)
+    form.matricular(usuario2)
+
+    form.mostrarInfoFormacao()
 }
